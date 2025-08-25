850bf36145d915295712012c1d2e60ce4b19f833
PRASHANNA RAI <prai931024@gmail.com>
2025-08-25 09:27:42 +0200

    updated  view of vscode extension

--- webview-ui/MarkdownRenderer.tsx
+++ webview-ui/MarkdownRenderer.tsx
@@ -3,45 +3,32 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Image } from "primereact/image";
 import Robot from "./robot.svg";
+import Markdown from "react-markdown";
+
 type MarkdownRendererProps = {
   content: string;
 };
 
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
   return (
-    <div className="prose max-w-none">
-        <Image className="inline" src={Robot} alt="Robot" width="28px" height="20px" />
-        <div className="inline">
-      <ReactMarkdown 
-        remarkPlugins={[remarkGfm]}
-        components={{
-          code({
-            node,
-            inline,
-            className,
-            children,
-            ...props
-          }: {
-            node: unknown;
-            inline?: boolean;
-            className?: string;
-            children: React.ReactNode;
-          }) {
-            return inline ? (
-              <code className={className} {...props}>
-                {children}
-              </code>
-            ) : (
-              <pre className={className} {...props}>
-                <code>{children}</code>
-              </pre>
-            );
-          },
-        }}
-      >
-        {content}
-      </ReactMarkdown>
-      </div>
+    // <div className="prose max-w-none">
+    //   <Image
+    //     className="inline"
+    //     src={Robot}
+    //     alt="Robot"
+    //     width="28px"
+    //     height="20px"
+    //   />
+    //   <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
+    // </div>
+
+    <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
+      <img
+        src={Robot}
+        alt="Description"
+        style={{ width: "28px", height: "20px" }}
+      />
+      <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
     </div>
   );
 };
